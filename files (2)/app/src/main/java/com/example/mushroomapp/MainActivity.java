05db2af644b50adf1369bcab30a150e558b005af
jeevikadlp-commits <jeevikadlp@gmail.com>
2025-10-03 13:55:53 -0700

    Add files via upload

---
+++ files (2)/app/src/main/java/com/example/mushroomapp/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.mushroomapp;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.loadUrl("https://studio--studio-5456289666-7a106.us-central1.hosted.app/");
+    }
+}
